030a3d8763aea08f75c3c2c713aca5fcc86bd40f
Michal Zuber <info@nevilleweb.sk>
2018-07-10 14:31:12 +0200

    update regex due rawdata change

--- Templates/Sk.php
+++ Templates/Sk.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Novutec Domain Tools
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@
 /**
  * @namespace Novutec\Whois\Parser\Templates
  */
+
 namespace Novutec\WhoisParser\Templates;
 
 use Novutec\WhoisParser\Templates\Type\Regex;
@@ -38,40 +39,63 @@ class Sk extends Regex
 {
 
     /**
-	 * Blocks within the raw output of the whois
-	 * 
-	 * @var array
-	 * @access protected
-	 */
-    protected $blocks = array(1 => '/admin-id(.*?)(?=tech-id)/is', 
-            2 => '/tech-id(.*?)(?=dns_name)/is', 3 => '/dns_name(.*?)$/is');
+     * Blocks within the raw output of the whois
+     *
+     * @var array
+     * @access protected
+     */
+    protected $blocks = array(
+        1 => '/\n\Contact(.*?)(?=Contact)/s',
+        2 => '/\n\nRegistrar(.*?)(?=Contact)/s',
+        3 => '/Domain(.*?)(?=\n\nRegistrar)/s',
+    );
 
     /**
-	 * Items for each block
-	 * 
-	 * @var array
-	 * @access protected
-	 */
+     * Items for each block
+     *
+     * @var array
+     * @access protected
+     */
     protected $blockItems = array(
-            1 => array('/admin-id(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:handle', 
-                    '/admin-name(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:name', 
-                    '/admin-address(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:address', 
-                    '/admin-telephone(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:phone', 
-                    '/admin-email(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:email', 
-                    '/admin-org.-ID(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:orgid'), 
-            
-            2 => array('/tech-id(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:handle', 
-                    '/tech-name(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:name', 
-                    '/tech-address(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:address', 
-                    '/tech-telephone(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:phone', 
-                    '/tech-email(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:email', 
-                    '/tech-org.-ID(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:orgid'), 
-            
-            3 => array('/dns_name(?>[\x20\t]*)(.+)$/im' => 'nameserver', 
-                    '/dns_ipv4(?>[\x20\t]*)(.+)$/im' => 'ips', 
-                    '/last-update(?>[\x20\t]*)(.+)$/im' => 'changed', 
-                    '/valid-date(?>[\x20\t]*)(.+)$/im' => 'expires', 
-                    '/domain-status(?>[\x20\t]*)(.+)$/im' => 'status'));
+        1 => array(
+            '/Contact:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:handle',
+            '/Name:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:name',
+            '/Street:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:address',
+            '/City:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:city',
+            '/Country Code:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:country',
+            '/Postal Code:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:zipcode',
+            '/Phone:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:phone',
+            '/Email:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:email',
+            '/Organization:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:organization',
+            '/Organization ID:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:orgid',
+            '/Created:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:created',
+            '/Updated:(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:updated',
+        ),
+
+        2 => array(
+            '/Registrar:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:handle',
+            '/Name:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:name',
+            '/Street:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:address',
+            '/Postal Code:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:zipcode',
+            '/City:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:city',
+            '/Country Code:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:country',
+            '/Postal Code:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:zipcode',
+            '/Phone:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:phone',
+            '/Email:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:email',
+            '/Organization:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:organization',
+            '/Organization ID:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:orgid',
+            '/Created:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:created',
+            '/Updated:(?>[\x20\t]*)(.+)$/im' => 'contacts:tech:updated',
+        ),
+
+        3 => array(
+            '/Nameserver:(?>[\x20\t]*)(.+)$/im' => 'nameserver',
+            '/Created:(?>[\x20\t]*)(.+)$/im' => 'created',
+            '/Updated:(?>[\x20\t]*)(.+)$/im' => 'changed',
+            '/Valid Until:(?>[\x20\t]*)(.+)$/im' => 'expires',
+            '/EPP status:(?>[\x20\t]*)(.+)$/im' => 'status',
+        ),
+    );
 
     /**
      * RegEx to check availability of the domain name
